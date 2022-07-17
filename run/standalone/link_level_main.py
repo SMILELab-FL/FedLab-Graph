a870878c8ff6a56b75b52b6c132a8e372dee28b3
Starry-Hu <starryhu@foxmail.com>
2022-07-17 22:01:34 +0800

    fix some details and use val data to get the best model to test

--- run/standalone/link_level_main.py
+++ run/standalone/link_level_main.py
@@ -8,12 +8,11 @@ from pathlib import Path
 import uuid
 
 BASE_DIR = Path(__file__).resolve().parents[2]
-sys.path.append("../../")
+sys.path.append(str(BASE_DIR))
 
 from run.utils.functional import random_seed_init
 from fedlab.utils.aggregator import Aggregators
 from fedlab.utils.serialization import SerializationTool
-from fedlab.utils.functional import get_best_gpu
 from model.model_builder import get_gnn
 from trainer.link_serial_trainer import LinkFullBatchSubsetSerialTrainer
 from data_preprocessing.dataloader.dataloader_link import LinkLevelPartitioner
@@ -23,7 +22,7 @@ from run.utils.transform_builder import get_transform
 parser = argparse.ArgumentParser(description="Standalone link training example")
 parser.add_argument("--task", type=str, default='link_level')
 parser.add_argument("--dataset", type=str, default='ciao')
-parser.add_argument("--data_root", type=str, default='../../data/')
+parser.add_argument("--data_root", type=str, default=BASE_DIR / 'data')
 parser.add_argument("--total_client", type=int, default=20)
 parser.add_argument("--com_round", type=int, default=50)
 parser.add_argument("--sample_ratio", type=float, default=1)
@@ -59,14 +58,14 @@ gs = LinkLevelPartitioner(data_name=args.dataset,
 
 # get model
 args.cuda = True if torch.cuda.is_available() else False
-device = get_best_gpu() if args.cuda else 'cpu'
-model = get_gnn(args, num_features=gs.num_features, num_classes=gs.num_classes).to(device)
+model = get_gnn(args, num_features=gs.num_features, num_classes=gs.num_classes)
+temp_model = copy.deepcopy(model)
 
 # FL settings
 num_per_round = int(args.total_client * args.sample_ratio)
 aggregator = Aggregators.fedavg_aggregate
 
-# fedlab setup
+# fedlab setup, load model in the best gpu
 trainer = LinkFullBatchSubsetSerialTrainer(model=model,
                                            client_dict=gs.data_local_dict,
                                            cuda=args.cuda,
@@ -79,22 +78,34 @@ trainer = LinkFullBatchSubsetSerialTrainer(model=model,
 # train procedure
 to_select = [i for i in range(total_client_num)]
 acc_list = []
-test_pre_round = 10
+test_freq = 10
+
+# best model pt
+checkpt_folder = BASE_DIR / f'trained_model_dict/{args.task}/{args.dataset}/'
+checkpt_folder.mkdir(parents=True, exist_ok=True)
+checkpt_file = checkpt_folder / f'{uuid.uuid4().hex}.pt'
+best_val_loss = 100
 
 for rd in range(args.com_round):
     model_parameters = SerializationTool.serialize_model(model)
-    # valid evaluate
-    loss, acc = trainer.evaluate(model_parameters, is_valid=True)
-    print("Before round{} - val loss: {:.4f}, acc: {:.2f}".format(rd, loss, acc))
-
-    # test evaluate
-    if rd % test_pre_round == 0 and rd != 0:
-        loss, acc = trainer.evaluate(model_parameters, is_valid=False)
-        acc_list.append(acc)
-        print("Round {} - test loss: {:.4f}, acc: {:.2f}".format(rd, loss, acc))
-
     # FL-train
     selection = random.sample(to_select, num_per_round)
     parameters_list = trainer.local_process(payload=[model_parameters],
                                             id_list=selection)
     SerializationTool.deserialize_model(model, aggregator(parameters_list))
+
+
+    # valid evaluate
+    val_loss, val_acc = trainer.evaluate(is_valid=True)
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(model.state_dict(), checkpt_file)
+
+    print("After round{} - val loss: {:.4f}, acc: {:.2f}".format(rd, val_loss, val_acc))
+
+    # test evaluate
+    if rd % test_freq == 0:
+        temp_model.load_state_dict(torch.load(checkpt_file))
+        temp_model_parameters = SerializationTool.serialize_model(temp_model)
+        test_loss, test_acc = trainer.evaluate(temp_model_parameters, is_valid=False)
+        print("After round{} - test loss: {:.4f}, acc: {:.2f}".format(rd, test_loss, test_acc))
--- run/standalone/node_level_main.py
+++ run/standalone/node_level_main.py
@@ -1,5 +1,4 @@
 import copy
-import os
 import argparse
 import random
 import sys
@@ -8,12 +7,11 @@ from pathlib import Path
 import uuid
 
 BASE_DIR = Path(__file__).resolve().parents[2]
-sys.path.append("../../")
+sys.path.append(str(BASE_DIR))
 
 from run.utils.functional import random_seed_init
 from fedlab.utils.aggregator import Aggregators
 from fedlab.utils.serialization import SerializationTool
-from fedlab.utils.functional import get_best_gpu
 from model.model_builder import get_gnn
 from trainer.node_serial_trainer import NodeFullBatchSubsetSerialTrainer
 from data_preprocessing.dataloader.dataloader_node import NodeLevelPartitioner
@@ -22,7 +20,7 @@ from data_preprocessing.dataloader.dataloader_node import NodeLevelPartitioner
 parser = argparse.ArgumentParser(description="Standalone training example")
 parser.add_argument("--task", type=str, default='node_level')
 parser.add_argument("--dataset", type=str, default='cora')
-parser.add_argument("--data_root", type=str, default='../../data/')
+parser.add_argument("--data_root", type=str, default=BASE_DIR / 'data')
 parser.add_argument("--total_client", type=int, default=20)
 parser.add_argument("--com_round", type=int, default=50)
 parser.add_argument("--sample_ratio", type=float, default=1)
@@ -51,14 +49,14 @@ gs = NodeLevelPartitioner(data_name=args.dataset,
 
 # get model
 args.cuda = True if torch.cuda.is_available() else False
-device = get_best_gpu() if args.cuda else 'cpu'
-model = get_gnn(args, num_classes=gs.num_classes, num_features=gs.num_features).to(device)
+model = get_gnn(args, num_classes=gs.num_classes, num_features=gs.num_features)
+temp_model = copy.deepcopy(model)
 
 # FL settings
 num_per_round = int(args.total_client * args.sample_ratio)
 aggregator = Aggregators.fedavg_aggregate
 
-# fedlab setup
+# fedlab setup, load model in the best gpu
 trainer = NodeFullBatchSubsetSerialTrainer(model=model,
                                            client_dict=gs.data_local_dict,
                                            cuda=args.cuda,
@@ -72,19 +70,32 @@ trainer = NodeFullBatchSubsetSerialTrainer(model=model,
 to_select = [i for i in range(total_client_num)]
 acc_list = []
 test_freq = 10
+
+# best model pt
+checkpt_folder = BASE_DIR / f'trained_model_dict/{args.task}/{args.dataset}/'
+checkpt_folder.mkdir(parents=True, exist_ok=True)
+checkpt_file = checkpt_folder / f'{uuid.uuid4().hex}.pt'
+best_val_loss = 100
+
 for rd in range(args.com_round):
     model_parameters = SerializationTool.serialize_model(model)
-    # valid evaluate
-    loss, acc = trainer.evaluate(model_parameters, is_valid=True)
-    print("Before round{} - val loss: {:.4f}, acc: {:.2f}".format(rd, loss, acc))
-
-    # test evaluate
-    if rd % test_freq == 0 and rd != 0:
-        loss, acc = trainer.evaluate(model_parameters, is_valid=False)
-        print("Round {} - test loss: {:.4f}, acc: {:.2f}".format(rd, loss, acc))
-
     # FL-train
     selection = random.sample(to_select, num_per_round)
     parameters_list = trainer.local_process(payload=[model_parameters],
                                             id_list=selection)
     SerializationTool.deserialize_model(model, aggregator(parameters_list))
+
+    # valid evaluate
+    val_loss, val_acc = trainer.evaluate(is_valid=True)
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        torch.save(model.state_dict(), checkpt_file)
+
+    print("After round{} - val loss: {:.4f}, acc: {:.2f}".format(rd, val_loss, val_acc))
+
+    # test evaluate
+    if rd % test_freq == 0:
+        temp_model.load_state_dict(torch.load(checkpt_file))
+        temp_model_parameters = SerializationTool.serialize_model(temp_model)
+        test_loss, test_acc = trainer.evaluate(temp_model_parameters, is_valid=False)
+        print("After round{} - test loss: {:.4f}, acc: {:.2f}".format(rd, test_loss, test_acc))
